c765adb413c6921ccf4b65ccd44a95d365d6c915
jitender94136 <jitender94136@gmail.com>
2017-09-08 15:20:28 +0530

    WEBSOCKET CODE FOR PRODUCTION IS COMMITTED

--- src/main/java/com/qlik_websocket_client/nvclient/EchoClient.java
+++ src/main/java/com/qlik_websocket_client/nvclient/EchoClient.java
@@ -17,6 +17,17 @@ package com.qlik_websocket_client.nvclient;
  * License.
  */
 import java.io.*;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -46,7 +57,7 @@ public class EchoClient
     /**
      * The echo server on websocket.org.
      */
-    private static final String SERVER = "ws://localhost:4848/app";
+    private static final String SERVER = "wss://analytics.1viewinsights.com:4747/app";
 
     /**
      * The timeout value in milliseconds for socket connection.
@@ -103,6 +114,8 @@ public class EchoClient
     {
         WebSocket ws =  new WebSocketFactory()
             .setConnectionTimeout(TIMEOUT)
+            .setSSLSocketFactory(setCertificateConfiguration())
+            .setVerifyHostname(false)
             .createSocket(SERVER)
             .addListener(new WebSocketAdapter() {
                 // A text message arrived from the server.
@@ -133,6 +146,7 @@ public class EchoClient
                 
             })
             .addExtension(WebSocketExtension.PERMESSAGE_DEFLATE)
+            .addHeader("X-Qlik-User","UserDirectory=internal; UserId=sa_engine")
             .connect();
         System.out.println(ws.getAgreedProtocol() +"  "+ws.isOpen());
         if(ws.isOpen()) {
@@ -141,6 +155,46 @@ public class EchoClient
         return ws;
     }
 
+    
+	public static  SSLSocketFactory setCertificateConfiguration() {
+					SSLSocketFactory sslSocketFactory = null;
+					String qlikCertificatePath = "E:/opt/certificates/analytics.1viewinsights.com/";
+					String qlikPassword = "testtest";
+						try {
+								/************** BEGIN Certificate Acquisition **************/
+								String certFolder = qlikCertificatePath.trim(); // This is a folder reference to
+								String proxyCert = certFolder + "client.jks"; // Reference to the
+								String proxyCertPass = qlikPassword.trim(); // This is the password to access
+								String rootCert = certFolder + "root.jks"; // Reference to the root
+								String rootCertPass = qlikPassword.trim(); // This is the password to access
+								System.out.println("certificate configured---------------------------------------");									
+								/************** END Certificate Acquisition **************/
+								/************** BEGIN Certificate configuration for use in connection **************/
+								KeyStore ks = KeyStore.getInstance("JKS");
+								ks.load(new FileInputStream(new File(proxyCert)),
+										proxyCertPass.toCharArray());
+								KeyManagerFactory kmf = KeyManagerFactory
+										.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+								kmf.init(ks, proxyCertPass.toCharArray());
+								SSLContext context = SSLContext.getInstance("SSL");
+								KeyStore ksTrust = KeyStore.getInstance("JKS");
+								ksTrust.load(new FileInputStream(rootCert),
+										rootCertPass.toCharArray());
+								TrustManagerFactory tmf = TrustManagerFactory
+										.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+								tmf.init(ksTrust);
+								context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+								sslSocketFactory = context.getSocketFactory();
+								/************** END Certificate configuration for use in connection **************/
+					    }	catch (KeyStoreException e) { e.printStackTrace(); } 
+					catch (IOException e) { e.printStackTrace(); } 
+					catch (CertificateException e) { e.printStackTrace(); } 
+					catch (NoSuchAlgorithmException e) { e.printStackTrace(); } 
+					catch (UnrecoverableKeyException e) { e.printStackTrace(); } 
+					catch (KeyManagementException e) { e.printStackTrace(); } 
+			return sslSocketFactory;
+			}
+    
 
     /**
      * Wrap the standard input with BufferedReader.
